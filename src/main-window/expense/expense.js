4073a0962c852c5916d6ee3ec5f08589e8beda5e
Leonardo Salpini <leonardo.salpini00@gmail.com>
2024-03-18 18:16:15 +0100

    aded alert for confirmation or error

--- src/main-window/expense/expense.js
+++ src/main-window/expense/expense.js
@@ -5,6 +5,16 @@ const installment_form = document.getElementById('installment-form');
 const installment_number = document.getElementById('installment-number');
 const select_categories = document.getElementById('expense-category');
 const submitButton = document.getElementById('submit');
+const confirmAlert = document.getElementById('confirm-alert');
+const errorAlert = document.getElementById('error-alert');
+
+errorAlert.style.display = 'none';
+confirmAlert.style.display = 'none';
+installment_number.disabled = true;
+
+installment_checkbox.addEventListener('change', (event) => {
+    installment_number.disabled = !installment_checkbox.checked;
+});
 
 window.electron.getSubjects().then(subjects => {
     subjects.forEach(subject => {
@@ -15,7 +25,6 @@ window.electron.getSubjects().then(subjects => {
     });
 });
 
-
 window.electron.getCategories().then(categories => {
     categories.forEach(category => {
         const option = document.createElement('option');
@@ -24,9 +33,6 @@ window.electron.getCategories().then(categories => {
         select_categories.appendChild(option);
     });
 })
-
-// Add event listener for submit button, when clicked, it will create a new expense object and
-// send it to the main process
 
 submitButton.addEventListener('click', async (event) => {
     event.preventDefault();
@@ -39,39 +45,74 @@ submitButton.addEventListener('click', async (event) => {
 })
 
 async function createExpense(year) {
-    const expense = {
-        id: await window.electron.getNextAvailableIdByYear(year),
-        subject: document.getElementById('expense-subject').value,
-        date: document.getElementById('expense-date').value,
-        type: document.getElementById('expense-category').value,
-        amount: document.getElementById('expense-amount').value,
-        description: document.getElementById('expense-description').value
-    }
-    window.electron.newExpense(expense, year).then(() => {
-        window.location.href = 'expense.html';
-    });
-}
-
-async function createInstallment() {
-    let date = new Date(document.getElementById('expense-date').value);
-    for(let i = 0; i < installment_number.value; i++) {
-        const installment = {
-            id: await window.electron.getNextAvailableIdByYear(date.getFullYear()),
+    confirmAlert.style.display = 'none';
+    errorAlert.style.display = 'none';
+    try {
+        const expense = {
+            id: await window.electron.getNextAvailableIdByYear(year),
             subject: document.getElementById('expense-subject').value,
-            date: date.toISOString().split('T')[0],
+            date: document.getElementById('expense-date').value,
             type: document.getElementById('expense-category').value,
             amount: document.getElementById('expense-amount').value,
-            description: document.getElementById('expense-description').value +
-                ` - Rata ${i + 1} di ${installment_number.value}`
+            description: document.getElementById('expense-description').value
         }
-        await window.electron.newExpense(installment, date.getFullYear());
-        date.setMonth(date.getMonth() + 1);
+        validateExpense(expense);
+        //await window.electron.newExpense(expense, year);
+        confirmAlert.style.display = 'block';
+    } catch (error) {
+        console.error(error);
+        errorAlert.style.display = 'block';
+        errorAlert.innerText = error.message;
+    } finally {
+        setTimeout(() => {
+            errorAlert.style.display = 'none';
+            confirmAlert.style.display = 'none';
+        }, 3000);
+    }
+}
+
+function validateExpense(expense) {
+    if (expense.subject === '' || expense.date === '' || expense.type === '' || expense.amount === '') {
+        throw new Error('Campi obbligatori non compilati');
+    } else if (isNaN(expense.amount)) {
+        throw new Error('L\'importo deve essere un numero');
+    }
+    if (!installment_number.disabled) {
+        if (installment_number.value === '' || isNaN(installment_number.value) || installment_number.value < 2) {
+            throw new Error('Numero di rate non valido');
+        }
     }
 }
 
 
-installment_number.disabled = true;
+async function createInstallment() {
+    try {
+        let date = new Date(document.getElementById('expense-date').value);
+        for (let i = 0; i < installment_number.value; i++) {
+            const installment = {
+                id: await window.electron.getNextAvailableIdByYear(date.getFullYear()),
+                subject: document.getElementById('expense-subject').value,
+                date: date.toISOString().split('T')[0],
+                type: document.getElementById('expense-category').value,
+                amount: document.getElementById('expense-amount').value,
+                description: document.getElementById('expense-description').value +
+                    ` - Rata ${i + 1} di ${installment_number.value}`
+            }
+            console.log(installment);
+            validateExpense(installment)
+            await window.electron.newExpense(installment, date.getFullYear());
+            date.setMonth(date.getMonth() + 1);
+        }
+        confirmAlert.style.display = 'block';
+    } catch (error) {
+        console.error(error);
+        errorAlert.style.display = 'block';
+        errorAlert.innerText = error.message;
+    } finally {
+        setTimeout(() => {
+            errorAlert.style.display = 'none';
+            confirmAlert.style.display = 'none';
+        }, 3000);
+    }
+}
 
-installment_checkbox.addEventListener('change', (event) => {
-    installment_number.disabled = !installment_checkbox.checked;
-});
