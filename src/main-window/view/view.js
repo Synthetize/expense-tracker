6ded344a5aead19d869f067290e4498886029a33
Leonardo Salpini <leonardo.salpini00@gmail.com>
2024-03-18 19:57:56 +0100

    added style for expense view

--- src/main-window/view/view.js
+++ src/main-window/view/view.js
@@ -4,7 +4,13 @@ const fromDate = document.getElementById('from-date');
 const toDate = document.getElementById('to-date');
 const applyButton = document.getElementById('apply-filter');
 const radioForm = document.getElementById('radio-form');
+const amountField = document.getElementById('total-amount');
 
+document.addEventListener('DOMContentLoaded', () => {
+    const firstRadioButton = document.querySelector('#radio-form input[type="radio"]:first-child');
+    firstRadioButton.checked = true;
+    radioForm.value = firstRadioButton.value;
+})
 
 window.electron.getYears().then(years => {
     years.forEach(year => {
@@ -15,19 +21,17 @@ window.electron.getYears().then(years => {
     });
     select.selectedIndex = select.options.length - 1;
     select.value = select.options[select.selectedIndex].value;
+    applyFilter()
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const firstRadioButton = document.querySelector('#radio-form input[type="radio"]:first-child');
-    firstRadioButton.checked = true;
-    radioForm.value = firstRadioButton.value;
-    applyButton.click();
-})
 
 
-applyButton.addEventListener('click', async () => {
-    console.log('apply button clicked');
-    console.log(select.value);
+const elements = [select, toDate, fromDate, radioForm];
+elements.forEach(element => {
+    element.addEventListener('change', applyFilter);
+});
+
+function applyFilter() {
     window.electron.getExpensesByYear(select.value).then(expenses => {
         if (fromDate.value !== '') {
             expenses = expenses.filter(expense => new Date(expense.date) >= new Date(fromDate.value));
@@ -58,11 +62,23 @@ applyButton.addEventListener('click', async () => {
         }
         updateTable(expenses);
     })
-})
+}
 
 function updateTable(expenses) {
     const table = document.getElementById('expenses-table');
     document.getElementById('table-body').innerHTML = '';
+    // Calcola la somma totale delle spese
+    const totalAmount = expenses.reduce((total, expense) => total + parseFloat(expense.amount), 0).toFixed(2);
+
+    // Crea una nuova riga per la somma totale
+    const totalRow = document.createElement('tr');
+    const totalCell = document.createElement('td');
+    totalCell.innerText = `Totale: ${totalAmount} €`;
+    totalCell.colSpan = 6; // Assumendo che la tua tabella abbia 6 colonne
+    totalRow.appendChild(totalCell);
+
+    // Aggiungi la riga della somma totale in cima alla tabella
+    document.getElementById('table-body').appendChild(totalRow);
     expenses.forEach(expense => {
         const row = document.createElement('tr');
         Object.keys(expense).forEach(key => {
