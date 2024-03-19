e21a165d5d6f4246dac02fd102051e5329201c02
Leonardo Salpini <leonardo.salpini00@gmail.com>
2024-03-19 13:24:04 +0100

    fixed table style for category-details

--- src/category-details/category-details.js
+++ src/category-details/category-details.js
@@ -2,15 +2,16 @@ let params = new URLSearchParams(window.location.search);
 let year = params.get('year');
 let category = params.get('category');
 const table = document.getElementById('category-expenses-table')
+const table_body = document.getElementById('table-body');
 
 window.electron.getCategoryDetailsByYear(year, category).then(expenses => {
     expenses.forEach(expense => {
         let row = document.createElement('tr');
-        for (let [key, value] of Object.entries(expense)) {
-            let cell = document.createElement('td');
-            cell.textContent = value.toString();
+        Object.keys(expense).forEach(key => {
+            const cell = document.createElement('td');
+            cell.innerText = expense[key];
             row.appendChild(cell);
-        }
-        table.appendChild(row);
+        })
+        table_body.appendChild(row);
     })
 })
